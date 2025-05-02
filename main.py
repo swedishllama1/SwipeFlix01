8927ad9b971cdf43bbbee39fbe32a7bfcad0a2d4
swedishllama1 <a.rangstrom@gmail.com>
2025-05-02 15:00:27 +0200

    fixat nyckeln och databaskoppling

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from dotenv import load_dotenv
 import os
+import psycopg2
 
 load_dotenv()
 
 tmdb_key = os.getenv("TMDB_API_KEY")
-print(f"API-nyckel: {tmdb_key}")
+
+def get_db_connection():
+        """Skapar och retunerar en databasanslutning"""
+        return psycopg2.connect (
+        dbname="aa_swipeflix",  # database name
+        user="aq1770",  # enter username
+        password="X0c5vfzn",  # enter password
+        host="pgserver.mau.se",  # enter database host
+        port="5432"  # standardporten för PostgreSQL
+    )
